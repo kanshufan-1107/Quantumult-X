fa998576cb744408ce641f531f50a411191a89f2
kanshufan <3891680418@qq.com>
2026-05-23 14:36:30 +0800

    修复 Cookie 检查条件，改为使用 p_skey

--- qq-cookie-save.js
+++ qq-cookie-save.js
@@ -3,7 +3,7 @@
 const headers = $request.headers;
 const cookie = headers["Cookie"] || headers["cookie"];
 
-if (cookie && cookie.includes("skey=")) {
+if (cookie && cookie.includes("p_skey=")) {
   $prefs.setValueForKey(cookie, "qq_cookie");
 
   // 提取 uin 显示
