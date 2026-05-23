6fe5f701688dc72a71a3c56ddb189657252e1a3c
kanshufan <3891680418@qq.com>
2026-05-23 14:40:17 +0800

    删除 qq-loader.js 文件

--- qq-loader.js
+++
@@ -1,9 +0,0 @@
-const timestamp = Date.now();
-const scriptUrl = `https://raw.githubusercontent.com/kanshufan-1107/Quantumult-X/refs/heads/main/qq-cookie-save.js?t=${timestamp}`;
-
-$task.fetch({ url: scriptUrl }).then(res => {
-  eval(res.body);
-  $done({});
-}, err => {
-  $done({});
-});
